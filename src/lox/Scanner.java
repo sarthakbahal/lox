7ced00dcbf67024c05a47cebc37af4b3256bf89d
sarthakbahal <sarthakbahal.45@gmail.com>
2024-04-27 19:09:37 +0530

    Added basic operator recognition
    Added basic operator recognition and lexical error case.

--- src/lox/Scanner.java
+++ src/lox/Scanner.java
@@ -52,8 +52,29 @@ private void scanToken() {
 	break;
 	case '*':addToken(STAR);
 	break;
+	
+	case '!':addToken(match('=')? BANG_EQUAL : BANG);
+	break;
+	case '=':addToken(match('=')? EQUAL_EQUAL : EQUAL);
+	break;
+	case '<':addToken(match('=')? LESS_EQUAL : LESS);
+	break;
+	case '>':addToken(match('=')? GREATER_EQUAL : GREATER);
+	break;
+	
+	default:
+		lox.error(line, " Unexpected character ");
+		break;
 	 
 	}
+}
+
+private boolean match(char exp) {
+	if(isAtEnd())return false;
+	if(source.charAt(current)!= exp) return false;
+	
+	current++;
+	return true;
 }
 
 private boolean isAtEnd() {
--- src/lox/lox.java
+++ src/lox/lox.java
@@ -8,7 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class Lox {
+public class lox {
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
