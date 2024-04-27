a5db2535393e30f7512392e9b4764a3e93796dfb
sarthakbahal <sarthakbahal.45@gmail.com>
2024-04-27 20:25:20 +0530

    Added cases for longer lexems
    Added methods like peek,match,etc for token disrribution.

--- src/lox/Scanner.java
+++ src/lox/Scanner.java
@@ -28,41 +28,61 @@ List<Token> scanTokens(){
 	tokens.add(new Token(EOF, "" , null, line));
 	return tokens;
 }
-
 private void scanToken() {
 	char c = advance();
 	switch(c) {
-	case '(':addToken(LEFT_PAREN);
-	break;
-	case ')':addToken(RIGHT_PAREN);
-	break;
-	case '{':addToken(LEFT_BRACE);
-	break;
-	case '}':addToken(RIGHT_BRACE);
-	break;
-	case ',':addToken(COMMA);
-	break;
-	case '.':addToken(DOT);
-	break;
-	case '-':addToken(MINUS);
-	break;
-	case '+':addToken(PLUS);
-	break;
-	case ';':addToken(SEMICOLON);
-	break;
-	case '*':addToken(STAR);
-	break;
+	 case '(':addToken(LEFT_PAREN);
+	 break;
+	 case ')':addToken(RIGHT_PAREN);
+	 break;
+	 case '{':addToken(LEFT_BRACE);
+	 break;
+	 case '}':addToken(RIGHT_BRACE);
+	 break;
+	 case ',':addToken(COMMA);
+	 break;
+	 case '.':addToken(DOT);
+	 break;
+	 case '-':addToken(MINUS);
+	 break;
+	 case '+':addToken(PLUS);
+	 break;
 	
-	case '!':addToken(match('=')? BANG_EQUAL : BANG);
-	break;
-	case '=':addToken(match('=')? EQUAL_EQUAL : EQUAL);
-	break;
-	case '<':addToken(match('=')? LESS_EQUAL : LESS);
-	break;
-	case '>':addToken(match('=')? GREATER_EQUAL : GREATER);
-	break;
+	 case ';':addToken(SEMICOLON);
+	 break;
+	 case '*':addToken(STAR);
+	 break;
 	
-	default:
+	 case '!':addToken(match('=')? BANG_EQUAL : BANG);
+	 break;
+	 case '=':addToken(match('=')? EQUAL_EQUAL : EQUAL);
+	 break;
+	 case '<':addToken(match('=')? LESS_EQUAL : LESS);
+	 break;
+	 case '>':addToken(match('=')? GREATER_EQUAL : GREATER);
+	 break;
+	
+	 case '/':
+		if(match('/')) {
+			while(peek()!= '\n' && !isAtEnd()) advance();
+		}
+		else {
+			addToken(SLASH);
+		}
+		break;
+		
+		
+	 case ' ':
+	 case '\r':
+	 case '\t':
+		break;
+		
+	 case '\n':
+		line++;
+		break;
+	
+	 
+	 default:
 		lox.error(line, " Unexpected character ");
 		break;
 	 
@@ -75,6 +95,12 @@ private boolean match(char exp) {
 	
 	current++;
 	return true;
+}
+
+private char peek() {
+	if(isAtEnd()) return '\0';
+	
+	return source.charAt(current);
 }
 
 private boolean isAtEnd() {
